40306f200c509e83f74edcd661e2695013b7705f
Michael Nesbitt <nesbitt@treeman.nesbitt.rocks>
2020-05-20 13:45:54 -0500

    Updated config settings

--- readConfig.cpp
+++ readConfig.cpp
@@ -15,129 +15,101 @@
 
 map<int, string> readConfig(string confFile)
 {
-  // Map for holding all the read config options.
-  map<int, string> configOptions;
+    // Map for holding all the read config options.
+    map<int, string> configOptions;
 
-  // // Create a rude config object
-  // rude::Config config;
+    // tao::config::value fileConfig;
 
-  // // Load the given config file
-  // if(config.load(confFile.c_str()))
-  // {
-  //   // Try to change to the parameters section, if it does not exists, this returns false.
-  //   if (config.setSection("Parameters", false))
-  //   {
-  //     // If the verbose flag is present in the config file
-  //     if (config.exists("Verbose"))
-  //     {
-  //       // If the Verbose value was true.
-  //       if (config.getBoolValue("Verbose"))
-  //       {
-  //         // Set the config verbosity to true.
-  //         configOptions[VERBOSE] = "true";
-  //       }
+    // fileConfig = tao::config::basic_from_file(confFile);
 
-  //       // If the Verbose value was false
-  //       else
-  //       {
-  //         // Set the config verbosity to false
-  //         configOptions[VERBOSE] = "false";
-  //       }
-  //     }
+    // Create a rude config object
+    rude::Config config;
 
-  //     // If the verbose flag was not present in the config file
-  //     else
-  //     {
-  //       // Write an error message to the screen
-  //       cerr << "Verbose flag missing from config file." << endl;
-  //       // Exit the program
-  //       exit(8);
-  //     }
+    // Load the given config file
+    if(config.load(confFile.c_str()))
+    {
+        // Try to change to the parameters section, if it does not exists, this returns false.
+        if (config.setSection("Daemon", false))
+        {
+            // If the verbose flag is present in the config file
+            if (config.exists("Verbose"))
+            {
+                // If the Verbose value was true.
+                if (config.getBoolValue("Verbose"))
+                {
+                    // Set the config verbosity to true.
+                    configOptions[VERBOSE] = "true";
+                }
 
-  //     // If the log file flag is preset in the config file
-  //     if (config.exists("LogFile"))
-  //     {
-  //       // Set the log file value
-  //       configOptions[LOG_FILE] = config.getStringValue("LogFile");
-  //     }
+                // If the Verbose value was false
+                else
+                {
+                    // Set the config verbosity to false
+                    configOptions[VERBOSE] = "false";
+                }
+            }
 
-  //     // If the log file flag was not present
-  //     else
-  //     {
-  //       // Write an error message to the screen
-  //       cerr << "LogFile flag missing from config file." << endl;
-  //       // Exit the program
-  //       exit(8);
-  //     }
+            // If the verbose flag was not present in the config file
+            else
+            {
+                // Write an error message to the screen
+                cerr << "Verbose flag missing from config file." << endl;
+                // Exit the program
+                exit(8);
+            }
 
-  //     // If the password flag is preset in the config file
-  //     if (config.exists("Password"))
-  //     {
-  //       // Set the passowrd value
-  //       configOptions[PASSWORD] = config.getStringValue("Password");
-  //     }
+            // If the log file flag is preset in the config file
+            if (config.exists("LogFile"))
+            {
+                // Set the log file value
+                configOptions[LOG_FILE] = config.getStringValue("LogFile");
+            }
 
-  //     // If the password flag was not present
-  //     else
-  //     {
-  //       // Write an error message to the screen
-  //       cerr << "Password flag missing from config file." << endl;
-  //       // Exit the program
-  //       exit(8);
-  //     }
+            // If the log file flag was not present
+            else
+            {
+                // Write an error message to the screen
+                cerr << "LogFile flag missing from config file." << endl;
+                // Exit the program
+                exit(8);
+            }
 
-  //     // If the num versions flag is preset in the config file
-  //     if (config.exists("NumVersions"))
-  //     {
-  //       // Set the num versions value
-  //       configOptions[NUM_VERSIONS] = config.getStringValue("NumVersions");
-  //     }
+            // If the java-path flag is preset in the config file
+            if (config.exists("Java-Path"))
+            {
+                // Set the java-path value
+                configOptions[JAVA_PATH] = config.getStringValue("Java-Path");
+            }
 
-  //     // If the num versions flag was not present
-  //     else
-  //     {
-  //       // Write an error message to the screen
-  //       cerr << "NumVersions flag missing from config file." << endl;
-  //       // Exit the program
-  //       exit(8);
-  //     }
+            // If the java-path flag was not present
+            else
+            {
+                // Write an error message to the screen
+                cerr << "Password flag missing from config file." << endl;
+                // Exit the program
+                exit(8);
+            }
+        }
 
-  //     // If the watch dir flag is preset in the config file
-  //     if (config.exists("WatchDir"))
-  //     {
-  //       // Set the watch dir value
-  //       configOptions[WATCH_DIR] = config.getStringValue("WatchDir");
-  //     }
+        // If the parameters section is missing
+        else
+        {
+            // Write an error message to the screen
+            cerr << "Daemon section missing from config file." << endl;
+            // Exit the program
+            exit(9);
+        }
+    }
 
-  //     // If the watch dir flag was not present
-  //     else
-  //     {
-  //       // Write an error message to the screen
-  //       cerr << "WatchDir flag missing from config file." << endl;
-  //       // Exit the program
-  //       exit(8);
-  //     }
-  //   }
+    // If there was an error opening the config file
+    else
+    {
+        // Write an error message to the screen
+        cerr << "Error opening config file: " <<  config.getError() << endl;
+        // Exit the program
+        exit(7);
+    }
 
-  //   // If the parameters section is missing
-  //   else
-  //   {
-  //     // Write an error message to the screen
-  //     cerr << "Parameters section missing from config file." << endl;
-  //     // Exit the program
-  //     exit(9);
-  //   }
-  // }
-
-  // // If there was an error opening the config file
-  // else
-  // {
-  //   // Write an error message to the screen
-  //   cerr << "Error opening config file: " <<  config.getError() << endl;
-  //   // Exit the program
-  //   exit(7);
-  // }
-
-  // Return the built map
-  return configOptions;
+    // Return the built map
+    return configOptions;
 }
