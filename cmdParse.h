ae67e3026e2398b497425eac8522edd03b6a082a
Michael Nesbitt <nesbitt@treeman.nesbitt.rocks>
2020-05-28 12:40:48 -0500

    Added macro definitions for versions

--- cmdParse.h
+++ cmdParse.h
@@ -11,9 +11,21 @@
 #define __CMDPARSE_H_
 
 #include <tclap/CmdLine.h>
+
+// Check if the current build is through CMake
 #ifdef CMAKE_BUILD
 #include "versionConfig.h"
 #endif
+
+// Check if the version has been set
+#ifndef __VERSION_MAJOR_
+#define __VERSION_MAJOR_ '?'
+#endif
+
+#ifndef __VERSION_MINOR_
+#define __VERSION_MINOR_ '?'
+#endif
+
 #include "main.h"
 
 map<int, string> cmdParse(int argc, char** argv);
