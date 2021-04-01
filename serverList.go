01f46c682a1e89a030d240145d5301d839c4640e
Michael Nesbitt <nesbitt@treeman.nesbitt.rocks>
2021-04-01 11:23:28 -0500

    Moved types to separate file

--- serverList.go
+++ serverList.go
@@ -2,37 +2,7 @@ package main
 
 import (
 	"container/list"
-	"io"
 )
-
-type requestServer struct {
-	Name        string `json:"name"`
-	Directory   string `json:"directory"`
-	JarFile     string `json:"jar_file"`
-	RunMemory   string `json:"run_memory"`
-	StartMemory string `json:"start_memory"`
-	JavaArgs    string `json:"java_args"`
-}
-
-type runningServer struct {
-	ID    int
-	Stdin io.WriteCloser
-	Log   *ServerLog
-}
-
-type runningServerList struct {
-	servers *list.List
-}
-
-func (l runningServerList) Find(i int) *list.Element {
-	for e := l.servers.Front(); e != nil; e = e.Next() {
-		if e.Value.(runningServer).ID == i {
-			return e
-		}
-	}
-
-	return nil
-}
 
 func newRunningServerList() *runningServerList {
 	return &runningServerList{list.New()}
---
+++ types.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"container/list"
+	"database/sql"
+	"io"
+)
+
+type databaseServer struct {
+	ID          int            `json:"id"`
+	Name        string         `json:"name"`
+	Directory   string         `json:"directory"`
+	JarFile     string         `json:"jar_file"`
+	RunMemory   string         `json:"run_memory"`
+	StartMemory string         `json:"start_memory"`
+	JavaArgs    sql.NullString `json:"java_args"`
+}
+
+type responseServer struct {
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Directory   string `json:"directory"`
+	JarFile     string `json:"jar_file"`
+	RunMemory   string `json:"run_memory"`
+	StartMemory string `json:"start_memory"`
+	JavaArgs    string `json:"java_args"`
+}
+
+type requestServer struct {
+	Name        string `json:"name"`
+	Directory   string `json:"directory"`
+	JarFile     string `json:"jar_file"`
+	RunMemory   string `json:"run_memory"`
+	StartMemory string `json:"start_memory"`
+	JavaArgs    string `json:"java_args"`
+}
+
+type runningServer struct {
+	ID    int
+	Stdin io.WriteCloser
+	Log   *ServerLog
+}
+
+type runningServerList struct {
+	servers *list.List
+}
+
+func (l runningServerList) Find(i int) *list.Element {
+	for e := l.servers.Front(); e != nil; e = e.Next() {
+		if e.Value.(runningServer).ID == i {
+			return e
+		}
+	}
+
+	return nil
+}
